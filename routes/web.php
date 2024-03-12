72611c507223eec11a3df9fb7ca7264da539edf0
Gevann Mullins <gevann.mullins@gmail.com>
2024-03-12 19:10:09 +0200

    Added/Updated the following: - created the env file - updated the composer.json file to use my unit-conversion-objects repo - created the login, register and home page views - created the Auth and Home controllers - created the Permissions, Role and ConversionHistory models - added the login, register and home views to the routes Register, Login and Home views tested and works

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\HomeController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,4 +18,20 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/test', function(){
+    return "Testing";
+});
+
+Route::group(['middleware' => 'guest'], function () {
+    Route::get('/register', [AuthController::class, 'register'])->name('register');
+    Route::post('/register', [AuthController::class, 'registerPost'])->name('register');
+    Route::get('/login', [AuthController::class, 'login'])->name('login');
+    Route::post('/login', [AuthController::class, 'loginPost'])->name('login');
+});
+ 
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/home', [HomeController::class, 'index']);
+    Route::delete('/logout', [AuthController::class, 'logout'])->name('logout');
 });
